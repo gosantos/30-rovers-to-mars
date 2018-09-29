c077ff0969b3ba24aa23b09c89418036153c8986
turao <lenz.arthur@gmail.com>
2018-09-29 10:04:17 -0300

    adding unit tests to plateau (position validation)

--- src/tests/test_plateau.py
+++ src/tests/test_plateau.py
@@ -1,5 +1,6 @@
 import unittest
 from marsrovers.plateau import Plateau
+from marsrovers.position import Position, Direction
 
 class TestPlateau(unittest.TestCase):
     def test_object_instantiation(self):
@@ -9,7 +10,7 @@ class TestPlateau(unittest.TestCase):
     @unittest.expectedFailure
     def test_invalid_object_instantiation_invalid_x_char(self):
         plateau = Plateau(x='someRandomStuff', y=2)
-    
+
     @unittest.expectedFailure
     def test_invalid_object_instantiation_invalid_negative_x(self):
         plateau = Plateau(x=-1, y=2)
@@ -17,7 +18,7 @@ class TestPlateau(unittest.TestCase):
     @unittest.expectedFailure
     def test_invalid_object_instantiation_invalid_y_char(self):
         plateau = Plateau(x=1, y='someRandomStuff')
-    
+
     @unittest.expectedFailure
     def test_invalid_object_instantiation_invalid_negative_y(self):
         plateau = Plateau(x=1, y=-2)
@@ -37,8 +38,27 @@ class TestPlateau(unittest.TestCase):
     def test_instantiation_from_invalid_string_with_wrong_y(self):
         str_plt = '1 someRandomInt'  # x, y, and direction
         plateau_from_string = Plateau.from_string(str_plt)
-    
+
     @unittest.expectedFailure
     def test_instantiation_from_invalid__with_wrong_argnumber(self):
         str_plt = '1'  # x, y, and direction
         plateau_from_string = Plateau.from_string(str_plt)
+
+    def test_position_validation(self):
+        plateau = Plateau(x=10, y=10)
+        position = Position(x=5, y=5, direction=Direction.NORTH)
+        self.assertTrue(plateau.isValidPosition(position))
+
+    def test_invalid_position_validation_x_out_of_bounds(self):
+        plateau = Plateau(x=10, y=10)
+        position = Position(x=99, y=5, direction=Direction.NORTH)
+        self.assertFalse(plateau.isValidPosition(position))
+        position = Position(x=-1, y=5, direction=Direction.NORTH)
+        self.assertFalse(plateau.isValidPosition(position))
+
+    def test_invalid_position_validation_y_out_of_bounds(self):
+        plateau = Plateau(x=10, y=10)
+        position = Position(x=5, y=99, direction=Direction.NORTH)
+        self.assertFalse(plateau.isValidPosition(position))
+        position = Position(x=5, y=-1, direction=Direction.NORTH)
+        self.assertFalse(plateau.isValidPosition(position))
